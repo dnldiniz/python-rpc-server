c777a0378fd5cded825e2839969c488a7ada8de9
Daniel Diniz <shadowelement@gmail.com>
2019-02-20 17:49:02 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import logging
+
+LOG = logging
+LOGGING_LEVEL_LOGFILE = LOG.INFO
+LOGGING_LEVEL_CONSOLE = LOG.INFO
+LOGGING_FILENAME = '/var/log/rpcs.log'
+
+MYSQL_DATABASE = 'db'
+MYSQL_USER = 'dbadmin'
+MYSQL_PASSWORD = 'h662sQ#k'
+
+RPCS_INTERFACE = '0.0.0.0'
+RPCS_PORT = 9116
+RPCS_TIMEOUT = 20
